7840570558058226d626bbb489e7ecf9ace6a023
Jian Han <jianhan@mail.com>
2020-08-29 10:24:51 +1000

    finished the monad js example

--- monad/monad.js
+++ monad/monad.js
@@ -13,13 +13,119 @@ class Maybe {
     }
 
     map(f) {
-        return new Maybe(f(value))
+        return new Maybe(f(this.$value))
+    }
+
+    ap(otherMaybe) {
+        return otherMaybe.map(this.$value)
+    }
+
+    chain(f) {
+        return this.map(f).join()
     }
 
     inspect() {
         return this.isNothing ? 'Nothing' : `Just(${this.$value})`
     }
+
+    join() {
+        return this.$value
+    }
+
+    orElse(v) {
+        return this.isNothing ? Maybe.of(v) : this
+    }
 }
 
-const m = Maybe.of(5)
-console.log(m.inspect())
+const validUser = {
+    email: 'james@example.com',
+    accountDetails: {
+        address: {
+            street: '123 Fake St',
+            city: 'Exampleville',
+            province: 'NS',
+            postcode: '1234'
+        }
+    },
+    preferences: {}
+}
+
+const banners = {
+    'AB': '/assets/banners/alberta.jpg',
+    'BC': '/assets/banners/british-columbia.jpg',
+    'MB': '/assets/banners/manitoba.jpg',
+    'NL': '/assets/banners/newfoundland-labrador.jpg',
+    'NS': '/assets/banners/nova-scotia.jpg',
+    'NT': '/assets/banners/northwest-territories.jpg',
+    'ON': '/assets/banners/ontario.jpg',
+    'PE': '/assets/banners/prince-edward.jpg',
+    'QC': '/assets/banners/quebec.jpg',
+    'SK': '/assets/banners/saskatchewan.jpg',
+    'YT': '/assets/banners/yukon.jpg',
+};
+
+const invalidUser = {}
+
+const getUserBannerImperative = (user, banners) => banners[user.accountDetails.address.province]
+
+const getUserBannerImperativeWithChecking = (user, banners) => {
+    if (user !== null) {
+        if (user.accountDetails !== undefined) {
+            if (user.accountDetails.address !== undefined) {
+                return banners[user.accountDetails.address.province];
+            }
+        }
+    }
+}
+
+var R = require('ramda'),
+    compose = R.compose,
+    prop = R.prop,
+    path = R.path;
+
+
+
+const getProvinceBanner = function (province) {
+    return Maybe.of(banners[province])
+}
+
+// const r = getUserBanner(banners, validUser)
+// console.log(r.inspect())
+
+// const p = getProvinceBanner('test')
+
+function getUserBanner(user) {
+    return Maybe.of(user)
+        .map(prop('accountDetails'))
+        .map(prop('address'))
+        .map(prop('province'))
+        .chain(getProvinceBanner);
+}
+
+const pp = getUserBanner(validUser)
+console.debug(pp)
+
+// Provide a default banner with .orElse()
+var bannerSrc = getUserBanner(validUser)
+    .orElse('/assets/banners/default-banner.jpg');
+
+// Grab the banner element and wrap it in a Maybe too.
+var bannerEl = Maybe.of(document.querySelector('.banner > img'));
+
+var applyBannerImperative = function (mBanner, mEl) {
+    mEl.__value.src = mBanner.__value;
+    return mEl;
+};
+
+applyBannerImperative(bannerSrc, bannerEl);
+
+var curry = require('ramda').curry;
+
+var applyBanner = curry(function (el, banner) {
+    el.src = banner;
+    return el;
+});
+
+const rrr = bannerEl.map(applyBanner)
+
+console.log(rrr)
